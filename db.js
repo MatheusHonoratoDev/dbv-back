02ad44c309212dba311f01c4c3f85528447a6b7a
Matheus Honorato de Sousa <102932606+MatheusHonoratoDev@users.noreply.github.com>
2024-01-31 18:25:21 -0300

    correlçoes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-import mysql from "mysql"
+import mysql2 from "mysql2";
 
-export const db = mysql.createConnection({
+export const db = mysql2.createConnection({
     host: "sql.freedb.tech",
     user: "freedb_bdclube-user",
     password: "Ca*qBXp$&9bHq9R",

421f6296119926626ccee0ef28dcd5351b207236
Matheus Honorato de Sousa <102932606+MatheusHonoratoDev@users.noreply.github.com>
2024-01-31 11:39:36 -0300

    bd

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "matheus157",
-    database: "bdclube"
+    host: "sql.freedb.tech",
+    user: "freedb_bdclube-user",
+    password: "Ca*qBXp$&9bHq9R",
+    database: "freedb_bdclube"
 });

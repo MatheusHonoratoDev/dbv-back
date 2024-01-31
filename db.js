2a492a92a9c93e1eac67740e89723d225238d25c
Matheus Honorato de Sousa <102932606+MatheusHonoratoDev@users.noreply.github.com>
2024-01-31 18:48:41 -0300

    testes

--- db.js
+++ db.js
@@ -1,17 +1,42 @@
-import mysql2 from "mysql2";
+const mysql2 = require('mysql2/promise');
 
-export const db = mysql2.createConnection({
+// Configurações do banco de dados
+const dbConfig = {
     host: "sql.freedb.tech",
     user: "freedb_bdclube-user",
     password: "Ca*qBXp$&9bHq9R",
-    database: "freedb_bdclube"
-});
+    database: "freedb_bdclube",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
 
-db.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.log('Conexão com o banco de dados perdida. Tentando reconectar...');
-        db.connect();
-    } else {
-        throw err;
+const pool = mysql2.createPool(dbConfig);
+
+async function executeQuery(sql, values) {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+
+        const [rows, fields] = await connection.execute(sql, values);
+
+        return rows;
+    } catch (error) {
+        throw error;
+    } finally {
+        if (connection) {
+            connection.release();
+        }
     }
-});
+}
+
+async function exemplo() {
+    try {
+        const resultado = await executeQuery("SELECT * FROM sua_tabela");
+        console.log(resultado);
+    } catch (erro) {
+        console.error("Erro ao executar a consulta:", erro);
+    }
+}
+
+exemplo();
